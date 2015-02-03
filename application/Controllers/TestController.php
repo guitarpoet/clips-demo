d208f6ec7bb095a4870fe6169f98e72bc770e9de
Jack <guitarpoet@gmail.com>
2015-02-03 09:46:22 +0800

    Added the view test

--- application/Controllers/TestController.php
+++ application/Controllers/TestController.php
@@ -1,6 +1,22 @@
 <?php namespace Demo\Controllers; in_array(__FILE__, get_included_files()) or exit("No direct sript access allowed");
 
-class TestController {
+use Clips\Controller;
+use Clips\Resource;
+
+class TestController extends Controller {
 	public function index() {
+		$data = array("world" => "Jack");
+		$engine = null;
+		if(isset($_GET['mus']))
+			$engine = 'mustache';
+		if(isset($_GET['mustache'])) {
+			return $this->render("welcome_mustache", $data, 'mustache');
+		}
+		return $this->render("welcome", $data, $engine);
+	}
+
+	/** @Clips\Rules("sample") */
+	public function rule() {
+		$this->clips->assertFacts(array("hello", "world"));
 	}
 }
