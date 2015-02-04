def7f2b4de5a1f605e235d74024d03c82f03296c
andy <andy.ling@pinet.co>
2015-02-04 13:20:14 +0800

    Change widget test

---
+++ application/Controllers/WidgetController.php
@@ -0,0 +1,16 @@
+<?php namespace Demo\Controllers; in_array(__FILE__, get_included_files()) or exit("No direct sript access allowed");
+
+use Clips\Controller;
+use Clips\Resource;
+
+class WidgetController extends Controller {
+	/**
+	 * @Clips\Js({"https://code.jquery.com/jquery-2.1.3.js"})
+	 * @Clips\Widget({"Html", "Demo"})
+	 * @Clips\Scss({"test"})
+	 */
+	public function index() {
+		$data = array("world" => "Jack");
+		return $this->render("tests/index", $data, $engine);
+	}
+}
