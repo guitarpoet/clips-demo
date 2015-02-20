aff89cbb1279b0eb4e1999b4a63df3ce3cf09182
Jack <guitarpoet@gmail.com>
2015-02-20 20:47:24 +0800

    Added the fake data to test DataTable.

--- migrations/20150215040038_demo_migration.php
+++ migrations/20150215040038_demo_migration.php
@@ -16,7 +16,14 @@ class DemoMigration extends AbstractMigration {
     public function up() {
 		$this->init();
 		$this->template->up($this, $this->config);
-		$this->template->insert($this, 'demo_groups', array(array('name' => 'admin')));
+		$group_id = $this->template->insert($this, 'demo_groups', array(array('name' => 'admin')));
+		$this->group_id = $group_id[0];
+
+		$this->fake_data = $this->tool->load_class('FakeData', true);
+
+		Clips\n_times(40, function() {
+			$this->template->insert($this, 'demo_users', array(array('username' => $this->fake_data->fakeName()->simple_name, 'group_id' => $this->group_id)));
+		});
     }
 
     /**
@@ -25,6 +32,7 @@ class DemoMigration extends AbstractMigration {
     public function down() {
 		$this->init();
 		$this->template->down($this, $this->config);
+		$this->execute('delete from demo_users');
 		$this->execute('delete from demo_groups');
     }
 }
