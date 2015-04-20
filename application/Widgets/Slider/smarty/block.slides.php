0981dce33f7ab37ae2210b668396d48007ee991c
andy <andy.ling@pinet.co>
2015-04-20 16:02:23 +0800

    Change slider

--- application/Widgets/Slider/smarty/block.slides.php
+++ application/Widgets/Slider/smarty/block.slides.php
@@ -2,16 +2,22 @@
 
 \Clips\require_widget_smarty_plugin('html', 'div');
 
-function smarty_block_slider($params, $content = '', $template, &$repeat) {
+function smarty_block_slides($params, $content = '', $template, &$repeat) {
 	if($repeat) {
 		\Clips\clips_context('indent_level', 1, true);
 		return;
 	}
-	
+
 	$default = array(
-		'class' => 'slider'
+		'u' => 'slides'
 	);
 
+	$true = true;
+	$content = "\t".Clips\process_list_items($params, $content, $template);
+
+	if(isset($params['items']))
+		unset($params['items']);
+	
 	\Clips\context_pop('indent_level');
 	return \Clips\create_tag('div', $params, $default, $content);
 }
